1d7254240b1802ebd278bb6f4879583a4e1861f2
Fifolu Akinyemi <fkinyemi@gmail.com>
2021-07-18 09:40:39 +0100

    Connected to MongoDB

--- app.js
+++ app.js
@@ -1,6 +1,18 @@
+const e = require('express');
 const express = require('express');
+const mongoose = require('mongoose');
+const uri = "mongodb+srv://node_tutorial:nodetutorial@cluster0.buwvv.mongodb.net/node_tutorial?retryWrites=true&w=majority"
 
 const app = express();
+
+mongoose.connect(uri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+})
+.then(() => {
+  console.log('MongoDB Connected...')
+})
+.catch(err => console.log(err))
 
 //Routes
 app.get('/', (req, res) => {
---
+++ properties.js
@@ -0,0 +1,5 @@
+module.exports = {
+
+  DB_PORT: 5000,
+  DB_URL: "http://localhost"
+}
